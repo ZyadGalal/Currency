e670cac5fb0c6d817b8ad3a9d1b3c02d3a3ba147
ZyadGalal <dev_zyad_galal@yahoo.com>
2022-02-22 00:41:43 +0200

    setup pickerView for to , from text fields

--- Currency/Scene/HomeScene/View/HomeViewController.swift
+++ Currency/Scene/HomeScene/View/HomeViewController.swift
@@ -6,12 +6,92 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class HomeViewController: BaseWireframe<HomeViewModel> {
 
+    @IBOutlet weak var fromTextField: UITextField!
+    @IBOutlet weak var toTextField: UITextField!
+    @IBOutlet weak var fromCurrency: UITextField!
+    @IBOutlet weak var toCurrency: UITextField!
+    
+    lazy var fromPickerView: UIPickerView = {
+        return UIPickerView()
+    }()
+    let toPickerView: UIPickerView = {
+        return UIPickerView()
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.setupUI()
+        viewModel.viewDidLoad()
     }
 
+    func setupUI() {
+        fromTextField.inputView = fromPickerView
+        createDoneBtn(for: fromTextField)
+        
+        toTextField.inputView = toPickerView
+        createDoneBtn(for: toTextField)
+    }
+    func createDoneBtn (for textField : UITextField)
+    {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let done = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action:#selector(donePressedOfPickerView))
+        toolbar.setItems([done], animated: true)
+        textField.inputAccessoryView = toolbar
+    }
+    @objc func donePressedOfPickerView()
+    {
+        self.view.endEditing(true)
+    }
+    override func bind(viewModel: HomeViewModel) {
+        fromTextField.rx.controlEvent([.editingChanged])
+            .subscribe({ [weak self] _ in
+                guard let self = self else {return}
+                self.viewModel.fromTextFieldDataChanged(with: self.fromTextField.text!)
+            }).disposed(by: disposeBag)
+        
+        toTextField.rx.controlEvent([.editingChanged])
+            .subscribe({ [weak self] _ in
+                guard let self = self else {return}
+                self.viewModel.toTextFieldDataChanged(with: self.toTextField.text!)
+
+            }).disposed(by: disposeBag)
+        
+        viewModel.price.bind(to: fromCurrency.rx.text).disposed(by: disposeBag)
+        
+        viewModel.pickerItems.bind(to: fromPickerView.rx.itemTitles){_, item in
+            return "\(item)"
+            
+        }.disposed(by: disposeBag)
+        
+        fromPickerView.rx.itemSelected.subscribe{ event in
+            switch event {
+            case .next(let selected):
+                print(selected.row)
+            default:
+                break
+
+            }
+        }.disposed(by: disposeBag)
+        
+        toPickerView.rx.itemSelected.subscribe{ event in
+            switch event {
+            case .next(let selected):
+                print(selected.row)
+            default:
+                break
+            }
+        }.disposed(by: disposeBag)
+        
+        viewModel.pickerItems.bind(to: toPickerView.rx.itemTitles){_, item in
+            return "\(item)"
+            
+        }.disposed(by: disposeBag)
+    }
 }
+
--- Currency/Scene/HomeScene/ViewModel/HomeViewModel.swift
+++ Currency/Scene/HomeScene/ViewModel/HomeViewModel.swift
@@ -6,13 +6,39 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
-
-class HomeViewModel: BaseViewModel {
+protocol output {
+    var price: Observable<String> { get }
+}
+class HomeViewModel: BaseViewModel, output {
     
     let repository: HomeRepository
+    var price: Observable<String>
+    var pickerItems: Observable<[String]>
     
+    private var prices: BehaviorRelay<String> = .init(value: "zyad")
+    private let items: Observable<[String]> = Observable.of(["Row1", "Row2", "Row3"])
     init(repository: HomeRepository) {
+        self.price = prices.asObservable()
+        self.pickerItems = items
         self.repository = repository
+    }
+    
+    func viewDidLoad() {
+        _ = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(setPrice), userInfo: nil, repeats: true)
+    }
+    
+    @objc func setPrice() {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+          
+        prices.accept(String((0..<4).map{ _ in letters.randomElement()! }))
+    }
+    func fromTextFieldDataChanged(with string: String) {
+        print(string)
+    }
+    func toTextFieldDataChanged(with string: String) {
+        print(string)
     }
 }
