921d50a7741d8bdecf62c7b8e6d22ce4efa9e7b6
ZyadGalal <dev_zyad_galal@yahoo.com>
2022-02-23 23:06:40 +0200

    add (last 30 days) table view cell

---
+++ Currency/Scene/DetailsScene/View/Cells/LastDaysTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  LastDaysTableViewCell.swift
+//  Currency
+//
+//  Created by Zyad Galal on 23/02/2022.
+//
+
+import UIKit
+
+class LastDaysTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
